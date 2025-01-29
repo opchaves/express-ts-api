1c83a5504ab53882499551b0d0e3d56932ae5367
Paulo Chaves <opaulochaves@gmail.com>
2025-01-29 12:47:15 -0300

    add mongodb connectio and mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import { env } from "@/common/utils/envConfig";
 import { app, logger } from "@/server";
+import mongoose from "mongoose";
 
-const server = app.listen(env.PORT, () => {
-  const { NODE_ENV, HOST, PORT } = env;
-  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
-});
+let server: ReturnType<typeof app.listen>;
+
+mongoose
+  .connect(env.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+
+    server = app.listen(env.PORT, () => {
+      const { NODE_ENV, HOST, PORT } = env;
+      logger.info(`${NODE_ENV} server running on http://${HOST}:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  });
 
 const onCloseSignal = () => {
   logger.info("sigint received, shutting down");
-  server.close(() => {
+  server.close(async () => {
+    await mongoose.connection.close();
     logger.info("server closed");
     process.exit();
   });
